278f0016a304d97c957ce35af2b7ed7d619054e4
Ishaan488 <ishaanbajpai732004@gmail.com>
2025-02-18 21:33:55 +0530

    feat: Added login and signup pages for buyer and seller

--- homepage/home.js
+++ homepage/home.js
@@ -2,7 +2,8 @@ const loginbtn = document.getElementById("loginbtn");
 const signupbtn = document.getElementById("signupbtn");
 const optionBox = document.getElementById("optionBox");
 const optionBoxClosebtn = document.getElementById("optionBoxClosebtn");
-
+const sellerOptionButton=document.getElementById("sellerOptionButton");
+const buyerOptionButton=document.getElementById("buyerOptionButton");
 
 function optionBoxOpen() {
     optionBox.style.display = 'block';
@@ -10,12 +11,25 @@ function optionBoxOpen() {
 function optionBoxClose() {
     optionBox.style.display = 'none';
 }
+
 optionBoxClosebtn.addEventListener("click",() => {
     optionBoxClose();
 })
 loginbtn.addEventListener("click",()=>{
     optionBoxOpen();
+    sellerOptionButton.addEventListener("click",()=>{
+        window.location.href='../SellerLoginPage/sellerLogin.html';
+    });
+    buyerOptionButton.addEventListener("click",()=>{
+        window.location.href='../BuyerLoginPage/buyerLogin.html';
+    });
 })
 signupbtn.addEventListener("click",()=>{
     optionBoxOpen();
+    sellerOptionButton.addEventListener("click",()=>{
+        window.location.href='../SellerSignupPage/sellerSignup.html';
+    });
+    buyerOptionButton.addEventListener("click",()=>{
+        window.location.href='../BuyerSignupPage/buyerSignup.html';
+    });
 })
